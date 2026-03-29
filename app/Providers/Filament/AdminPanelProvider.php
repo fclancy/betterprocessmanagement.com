d9ac21b9715f3652ed3296dc926cb8aa8493d5ab
Ford Clancy <ford@clancy.com>
2026-03-29 18:51:58 -0400

    feat: add Filament AdminPanelProvider to register admin panel
    - Created AdminPanelProvider that bootstraps Filament panel with admin path
    - Added provider to config/app.php
    - This registers admin routes on boot

---
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use Filament\Facades\Filament;
+use Illuminate\Support\ServiceProvider;
+
+class AdminPanelProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        Filament::panel([
+            'id' => 'admin',
+            'path' => 'admin',
+            'discoverResources' => true,
+            'discoverPages' => true,
+            'discoverWidgets' => true,
+            'middleware' => ['web', 'auth'],
+            'authMiddleware' => ['auth'],
+            'resources' => [
+                'pages' => [
+                    \Filament\Pages\Dashboard::class,
+                ],
+            ],
+            'widgets' => [
+                \Filament\Widgets\AccountWidget::class,
+                \Filament\Widgets\FilamentInfoWidget::class,
+            ],
+        ]);
+    }
+}
--- config/app.php
+++ config/app.php
@@ -65,6 +65,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\Filament\AdminPanelProvider::class,
     ],
 
     'aliases' => [
