ed8289eb5894f65835514c45163a7252e260953f
Ford Clancy <ford@clancy.com>
2026-03-29 18:53:08 -0400

    config: add Filament facade alias
    Added 'Filament' => Filament\Facades\Filament::class to aliases for AdminPanelProvider to use.

--- config/app.php
+++ config/app.php
@@ -86,6 +86,7 @@ return [
         'Eloquent' => Illuminate\Database\Eloquent\Model::class,
         'Event' => Illuminate\Support\Facades\Event::class,
         'File' => Illuminate\Support\Facades\File::class,
+        'Filament' => Filament\Facades\Filament::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'Http' => Illuminate\Support\Facades\Http::class,
